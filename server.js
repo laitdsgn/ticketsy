c26d4d29dd482654c09782eb74c2d51925eaea00
laitdsgn <tomek08.konto@gmail.com>
2025-12-14 12:42:12 +0100

    ui finished

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use("/styles", express.static("styles"));
 app.set("view engine", "pug");
 app.set("views", "./views");
 app.get("/", (req, res) => {
-  res.render("home", { user: "xyz" });
+  res.render("home");
 });
 
 app.use("/login", loginRoutes);
